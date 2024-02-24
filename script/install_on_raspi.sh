efe3ac73e11c175dbf2e955ffaf07e45f3a0e10c
MATSUNAGA Takuya <matsunaga.takuya.mt2@is.naist.jp>
2024-02-24 22:48:15 +0900

    add enable I2C

--- script/install_on_raspi.sh
+++ script/install_on_raspi.sh
@@ -40,3 +40,6 @@ installed_python_version="python$("${installer_dir}/script/get_python_version.sh
 ln -s "/usr/local/lib/${installed_python_version}/dist-packages/upm/" "${base_dir}/grove.py/.venv/lib/${installed_python_version}/site-packages/upm"
 ln -s "/usr/local/lib/${installed_python_version}/dist-packages/mraa.py" "${base_dir}/grove.py/.venv/lib/${installed_python_version}/site-packages/mraa.py"
 ln -s "/usr/local/lib/${installed_python_version}/dist-packages/_mraa.so" "${base_dir}/grove.py/.venv/lib/${installed_python_version}/site-packages/_mraa.so"
+
+# enable I2C
+sudo raspi-config nonint do_i2c 0
